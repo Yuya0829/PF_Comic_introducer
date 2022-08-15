1510f7803a7d9227c70145c80edf51e239d7d8c3
Yuya0829 <yuya.0829fantasista@gmail.com>
2022-08-15 12:28:01 +0000

    PF sign_up,in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root to: "homes#top"
+
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+
 
 end
