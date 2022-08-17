12b49932fe8fae1fe987c9be01e10b06b0e00d5e
Yuya0829 <yuya.0829fantasista@gmail.com>
2022-08-17 04:50:05 +0000

    ~destroy fin

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -12,16 +12,37 @@ class Public::PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
 
   def edit
+    @post_image = PostImage.find(params[:id])
+    if@post_image.user == current_user
+      render :edit
+    else
+      redirect_to post_images_path
+    end
+  end
+
+  def update
+    @post_image = PostImage.find(params[:id])
+    @post_image.update(post_image_params)
+    if@post_image.save
+      flash[:notice] = "You have updated comic successfully."
+      redirect_to post_image_path(@post_image.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
 
   private
