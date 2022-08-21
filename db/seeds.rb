61aa2ed06f8f47fb26ac81f95a28d4804bc55599
Yuya0829 <yuya.0829fantasista@gmail.com>
2022-08-21 13:01:41 +0000

    admin start

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.find_or_create_by(email: 'z@z') do |admin|
+  admin.password = 'zzzzzz'
+end
