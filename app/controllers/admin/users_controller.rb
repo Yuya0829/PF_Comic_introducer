16dd8949b935fe70a031547f74cbe7c2b68f3372
Yuya0829 <yuya.0829fantasista@gmail.com>
2022-08-23 06:05:27 +0000

    admin user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,8 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
