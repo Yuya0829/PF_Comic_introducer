2d02c4c867772597a02091dddfa601ed7d4b8c3b
Yuya0829 <yuya.0829fantasista@gmail.com>
2022-09-03 11:41:59 +0000

    update

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -42,6 +42,7 @@ class Public::PostImagesController < ApplicationController
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
+    @post_comments = @post_image.post_comments.page(params[:page]).per(2)
   end
 
   def edit
